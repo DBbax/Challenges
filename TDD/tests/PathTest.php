87f0d51eced98362ba17cc7d874674466f822465
Diego Bussola <diego.bussola@grupposinergia.net>
2020-08-26 02:29:44 +0200

    add other tests

--- TDD/tests/PathTest.php
+++ TDD/tests/PathTest.php
@@ -31,6 +31,18 @@ final class PathTest extends TestCase
         $this->assertInstanceOf(Path::class, $path);
     }
 
+    public function test_can_be_created__with_uppercase_chars(): void
+    {
+        $path = new Path('/A/B');
+        $this->assertInstanceOf(Path::class, $path);
+    }
+
+    public function test_can_be_created__with_uppercase_and_lowercase_chars(): void
+    {
+        $path = new Path('/A/B/c/d');
+        $this->assertInstanceOf(Path::class, $path);
+    }
+
     public function test_can_change_dir__to_parent()
     {
         $path = new Path('/a/b/c/d');
@@ -76,28 +88,58 @@ final class PathTest extends TestCase
     // - - - - - - - - - - - - - - - - - - - - - - - -
     // CAN'T DO
 
-    public function test_cannot_be_created_with_empty_param(): void
+    public function test_cannot_be_created__with_empty_param(): void
     {
         $this->expectException(Exception::class);
         $path = new Path('');
     }
 
-    public function test_cannot_be_created_without_initial_rootpath(): void
+    public function test_cannot_be_created__without_initial_rootpath(): void
     {
         $this->expectException(Exception::class);
         $path = new Path('a/b/');
     }
 
-    public function test_cannot_be_created_with_two_dots(): void
+    public function test_cannot_be_created__with_two_dots(): void
     {
         $this->expectException(Exception::class);
         $path = new Path('..');
     }
 
-    public function test_cannot_be_created_with_two_dots_in_path(): void
+    public function test_cannot_be_created__with_three_or_more_dots(): void
+    {
+        $this->expectException(Exception::class);
+        $path = new Path('...');
+    }
+
+    public function test_cannot_be_created__with_two_dots_in_path(): void
     {
         $this->expectException(Exception::class);
         $path = new Path('/a/../b');
+    }
+
+    public function test_cannot_be_created__with_dot_in_folder_name(): void
+    {
+        $this->expectException(Exception::class);
+        $path = new Path('/a/b/.c');
+    }
+
+    public function test_cannot_be_created__with_number_chars(): void
+    {
+        $this->expectException(Exception::class);
+        $path = new Path('/a/123');
+    }
+
+    public function test_cannot_be_created__with_other_chars(): void
+    {
+        $this->expectException(Exception::class);
+        $path = new Path('/a/$%&');
+    }
+
+    public function test_cannot_change_dir__with_three_or_more_dots_in_path(): void
+    {
+        $this->expectException(Exception::class);
+        $path = new Path('/a/.../b');
     }
 
     // - - - - - - - - - - - - - - - - - - - - - - - -
